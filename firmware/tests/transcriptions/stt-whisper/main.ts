6f7d9501a573151f61ab18f736f171026ff6adc4
STC <stc1988@users.noreply.github.com>
2025-05-19 23:46:38 +0900

    migrate to ECMA 419 AudioIn

--- firmware/tests/transcriptions/stt-whisper/main.ts
+++ firmware/tests/transcriptions/stt-whisper/main.ts
@@ -1,17 +1,18 @@
 import config from 'mc/config'
 import STT from 'stt-whisper'
-import Resource from 'Resource'
+import Microphone from 'microphone'
 
 const token = config.token
 
-const audio = new Resource('speak.wav')
 if (!token || token === 'YOUR_API_KEY_HERE') throw new Error('API token is missing.')
 
+const microphone = new Microphone()
 const stt = new STT({
   apiKey: token,
 })
 
 try {
+  const audio = await microphone.record()
   const result = await stt.transcribe(audio)
   if (result.success === true) {
     trace(result.value)
